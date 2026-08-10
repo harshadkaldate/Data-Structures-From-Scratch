7f8c04d705450c49e69c41ec2db0870b8ace39fc
Harshad Kaldate <harshadkaldate1@gmail.com>
2026-08-10 17:51:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (12.59%) - LeetSync

---
+++ 69-sqrtx/sqrtx.c
@@ -0,0 +1,15 @@
+int mySqrt(int x) {
+    int l = 1, r = x, ans = 0;
+
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+
+        if (m <= x / m) {
+            ans = m;
+            l = m + 1;
+        } else
+            r = m - 1;
+    }
+
+    return ans;
+}
