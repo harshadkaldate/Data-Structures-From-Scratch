e8efb9a26bf8f87641402033b674e88ca3fc51e6
Harshad Kaldate <harshadkaldate1@gmail.com>
2026-09-03 13:29:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (12.16%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.c
@@ -0,0 +1,12 @@
+int firstUniqChar(char* s){
+    int count[26]={0};
+
+    for(int i=0;s[i];i++)
+        count[s[i]-'a']++;
+
+    for(int i=0;s[i];i++)
+        if(count[s[i]-'a']==1)
+            return i;
+
+    return -1;
+}
